78efd84fbf3598714bc513328d919768ed962410
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-14 23:07:19 +0300

    feat(processor|processing): add ThumbnailSize value object

---
+++ app/internal/processor/processing/domain/vo/thumbnail_size.go
@@ -0,0 +1,10 @@
+package vo
+
+// ThumbnailSize is a supported avatar variant size.
+type ThumbnailSize string
+
+const (
+	ThumbnailSize100  ThumbnailSize = "100x100"
+	ThumbnailSize300  ThumbnailSize = "300x300"
+	ThumbnailOriginal ThumbnailSize = "original"
+)
