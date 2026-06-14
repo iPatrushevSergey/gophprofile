c28be9555a50b51d9c7ac9476ec0dfa1f5e795db
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-14 21:41:57 +0300

    feat(pkg|presentation): add default HTTP log formatter
    Added DefaultLogFormatter that logs request metadata at info level and bodies at debug level.

---
+++ app/internal/pkg/presentation/http/middleware/logger/default_formatter.go
@@ -0,0 +1,21 @@
+package logger
+
+import "github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/application/port"
+
+// DefaultLogFormatter implements standard logging logic.
+type DefaultLogFormatter struct{}
+
+// Log writes a default log entry.
+func (f *DefaultLogFormatter) Log(log port.Logger, p LogParams) {
+	log.Info("HTTP request",
+		"uri", p.Ctx.Request().RequestURI,
+		"method", p.Ctx.Request().Method,
+		"duration", p.Duration,
+		"status", p.Ctx.Response().Status,
+		"size", p.Ctx.Response().Size,
+	)
+	log.Debug("HTTP request/response body",
+		"request_body", string(p.RequestBody),
+		"response_body", p.ResponseBody.String(),
+	)
+}
