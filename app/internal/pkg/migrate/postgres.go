b1a916f905d322b008e3095e2e9747de79716205
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-14 19:51:12 +0300

    feat(pkg|migrate): add postgres migration runner
    Added MigrationsGophprofileDir helper.

---
+++ app/internal/pkg/migrate/postgres.go
@@ -0,0 +1,38 @@
+// Package migrate applies SQL schema migrations for the GophProfile database.
+package migrate
+
+import (
+	"database/sql"
+	"fmt"
+	"path/filepath"
+	"runtime"
+
+	"github.com/pressly/goose/v3"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+// MigrationsGophprofileDir returns the path to migrations/gophprofile from repo layout.
+func MigrationsGophprofileDir() string {
+	_, filename, _, _ := runtime.Caller(0)
+	return filepath.Join(filepath.Dir(filename), "..", "..", "..", "..", "migrations", "gophprofile")
+}
+
+// PostgresUp applies all pending PostgreSQL migrations to the database.
+func PostgresUp(dsn, dir string) error {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return fmt.Errorf("open db: %w", err)
+	}
+	defer db.Close()
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("set dialect: %w", err)
+	}
+
+	if err := goose.Up(db, dir); err != nil {
+		return fmt.Errorf("goose up: %w", err)
+	}
+
+	return nil
+}
