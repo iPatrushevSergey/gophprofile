61eaf51d96f8d46f47b90f027869aa13e2734e2b
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-18 23:29:12 +0300

    feat(server|avatar): expose GC and outbox use cases in presentation port

--- app/internal/server/avatar/presentation/port/usecases.go
+++ app/internal/server/avatar/presentation/port/usecases.go
@@ -6,11 +6,13 @@ import (
 	appport "github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/application/port"
 )
 
-// AvatarUseCases provides avatar use cases to HTTP handlers.
+// AvatarUseCases provides avatar use cases to HTTP handlers and workers.
 type AvatarUseCases interface {
 	UploadUseCase() appport.UseCase[appdto.UploadAvatarInput, appdto.UploadAvatarOutput]
 	GetUseCase() appport.UseCase[appdto.GetAvatarInput, appdto.GetAvatarOutput]
 	GetMetadataUseCase() appport.UseCase[appdto.GetAvatarMetadataInput, appdto.AvatarMetadataOutput]
 	DeleteUseCase() appport.UseCase[appdto.DeleteAvatarInput, struct{}]
 	HealthUseCase() appport.UseCase[appdto.HealthCheckInput, appdto.HealthCheckOutput]
+	ExpireUploadingAvatarsUseCase() appport.UseCase[struct{}, struct{}]
+	PublishPendingOutboxEventsUseCase() appport.UseCase[struct{}, struct{}]
 }
