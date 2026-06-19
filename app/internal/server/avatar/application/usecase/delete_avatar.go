cda65ad204a2645ffb89bc4dc334d4e6790e2dc9
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-19 16:13:07 +0300

    feat(server|avatar): add DeleteAvatar use case
    Added use case that soft-deletes avatar and enqueues cleanup event with all S3 keys via outbox.

---
+++ app/internal/server/avatar/application/usecase/delete_avatar.go
@@ -0,0 +1,60 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/application"
+	"github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/application/dto"
+	appport "github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/application/port"
+)
+
+// DeleteAvatar soft-deletes avatar and enqueues cleanup event.
+type DeleteAvatar struct {
+	avatarReader appport.AvatarReader
+	avatarWriter appport.AvatarWriter
+	outboxWriter appport.OutboxWriter
+	transactor   appport.Transactor
+}
+
+// NewDeleteAvatar returns the delete avatar use case.
+func NewDeleteAvatar(
+	avatarReader appport.AvatarReader,
+	avatarWriter appport.AvatarWriter,
+	outboxWriter appport.OutboxWriter,
+	transactor appport.Transactor,
+) appport.UseCase[dto.DeleteAvatarInput, struct{}] {
+	return &DeleteAvatar{
+		avatarReader: avatarReader,
+		avatarWriter: avatarWriter,
+		outboxWriter: outboxWriter,
+		transactor:   transactor,
+	}
+}
+
+// Execute deletes an avatar.
+func (uc *DeleteAvatar) Execute(ctx context.Context, in dto.DeleteAvatarInput) (struct{}, error) {
+	avatar, err := uc.avatarReader.FindByID(ctx, in.AvatarID)
+	if err != nil {
+		return struct{}{}, err
+	}
+
+	if avatar.UserID != in.RequestUserID {
+		return struct{}{}, application.ErrForbidden
+	}
+
+	err = uc.transactor.RunInTransaction(ctx, func(txCtx context.Context) error {
+		if err := uc.avatarWriter.SoftDelete(txCtx, in.AvatarID, in.RequestUserID); err != nil {
+			return err
+		}
+
+		return uc.outboxWriter.EnqueueAvatarDeleted(txCtx, dto.AvatarDeletedEvent{
+			AvatarID: avatar.ID,
+			S3Keys:   avatar.AllS3Keys(),
+		})
+	})
+	if err != nil {
+		return struct{}{}, err
+	}
+
+	return struct{}{}, nil
+}
