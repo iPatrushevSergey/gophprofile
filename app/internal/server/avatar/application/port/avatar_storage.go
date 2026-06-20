8e1362e92734fb94c54c3990182353f984e9b1f4
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-20 12:43:49 +0300

    feat(server|avatar): add Ping to AvatarStorage port

--- app/internal/server/avatar/application/port/avatar_storage.go
+++ app/internal/server/avatar/application/port/avatar_storage.go
@@ -7,4 +7,5 @@ type AvatarStorage interface {
 	Put(ctx context.Context, key string, data []byte, contentType string) error
 	Get(ctx context.Context, key string) ([]byte, error)
 	Delete(ctx context.Context, key string) error
+	Ping(ctx context.Context) error
 }
