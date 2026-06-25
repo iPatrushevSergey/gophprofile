d5974c073ce20ea7271407a7ad213e6b709bc90e
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-25 19:08:00 +0300

    feat(processor|processing): add thumbnail size value object

--- app/internal/processor/processing/domain/vo/thumbnail_size.go
+++ app/internal/processor/processing/domain/vo/thumbnail_size.go
@@ -8,3 +8,13 @@ const (
 	ThumbnailSize300  ThumbnailSize = "300x300"
 	ThumbnailOriginal ThumbnailSize = "original"
 )
+
+// Valid reports whether the thumbnail size is known.
+func (s ThumbnailSize) Valid() bool {
+	switch s {
+	case ThumbnailSize100, ThumbnailSize300, ThumbnailOriginal:
+		return true
+	default:
+		return false
+	}
+}
