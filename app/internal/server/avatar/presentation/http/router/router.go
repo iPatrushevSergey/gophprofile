fc76378abecef745117944a92e71f8d6ba682c9b
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-25 13:18:58 +0300

    feat(server|avatar): add HTTP router with public and protected avatar routes

---
+++ app/internal/server/avatar/presentation/http/router/router.go
@@ -0,0 +1,27 @@
+// Package router registers avatar HTTP routes.
+package router
+
+import (
+	"github.com/labstack/echo/v4"
+
+	appport "github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/application/port"
+	"github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/presentation/http/handler"
+	presport "github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/presentation/port"
+)
+
+// RegisterAvatarRoutes registers avatar module HTTP routes.
+func RegisterAvatarRoutes(
+	r *echo.Echo,
+	public, protected *echo.Group,
+	useCases presport.AvatarUseCases,
+	log appport.Logger,
+) {
+	h := handler.NewAvatarHandler(useCases, log)
+
+	r.GET("/health", h.Health)
+
+	public.GET("/avatars/:avatar_id", h.Get)
+	public.GET("/avatars/:avatar_id/metadata", h.GetMetadata)
+	protected.POST("/avatars", h.Upload)
+	protected.DELETE("/avatars/:avatar_id", h.Delete)
+}
