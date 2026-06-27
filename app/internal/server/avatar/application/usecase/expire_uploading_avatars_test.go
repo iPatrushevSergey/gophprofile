c67c014d4e76a2de3b2f96976557a0a954552c9b
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-27 21:39:47 +0300

    test(server|avatar): update ExpireUploadingAvatars tests for continue-on-error

--- app/internal/server/avatar/application/usecase/expire_uploading_avatars_test.go
+++ app/internal/server/avatar/application/usecase/expire_uploading_avatars_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 
+	"github.com/iPatrushevSergey/gophprofile/app/internal/pkg/adapters/logger"
 	portmocks "github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/application/port/mocks"
 	"github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/domain/entity"
 	"github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/domain/vo"
@@ -18,6 +19,7 @@ func TestExpireUploadingAvatars_Execute(t *testing.T) {
 	ctx := context.Background()
 	now := time.Date(2026, 6, 13, 12, 0, 0, 0, time.UTC)
 	avatar := entity.NewAvatar("avatar-1", "user-1", "avatar.png", "image/png", 5, "user-1/avatar-1/original", vo.UploadStatusUploading, now)
+	other := entity.NewAvatar("avatar-2", "user-1", "avatar2.png", "image/png", 5, "user-1/avatar-2/original", vo.UploadStatusUploading, now)
 
 	t.Run("expiresUploading", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
@@ -31,12 +33,12 @@ func TestExpireUploadingAvatars_Execute(t *testing.T) {
 		storage.EXPECT().Delete(ctx, "user-1/avatar-1/original").Return(nil)
 		writer.EXPECT().MarkUploadFailed(ctx, "avatar-1", now).Return(nil)
 
-		uc := NewExpireUploadingAvatars(reader, writer, storage, clock, 30*time.Minute)
+		uc := NewExpireUploadingAvatars(reader, writer, storage, clock, logger.NewNopLogger(), 30*time.Minute)
 		_, err := uc.Execute(ctx, struct{}{})
 		require.NoError(t, err)
 	})
 
-	t.Run("returnsErrorWhenDeleteFails", func(t *testing.T) {
+	t.Run("continuesWhenDeleteFails", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		reader := portmocks.NewMockAvatarReader(ctrl)
 		writer := portmocks.NewMockAvatarWriter(ctrl)
@@ -44,11 +46,13 @@ func TestExpireUploadingAvatars_Execute(t *testing.T) {
 		clock := portmocks.NewMockClock(ctrl)
 
 		clock.EXPECT().Now().Return(now)
-		reader.EXPECT().ListExpiredUploading(ctx, now.Add(-30*time.Minute)).Return([]entity.Avatar{*avatar}, nil)
+		reader.EXPECT().ListExpiredUploading(ctx, now.Add(-30*time.Minute)).Return([]entity.Avatar{*avatar, *other}, nil)
 		storage.EXPECT().Delete(ctx, "user-1/avatar-1/original").Return(errors.New("storage unavailable"))
+		storage.EXPECT().Delete(ctx, "user-1/avatar-2/original").Return(nil)
+		writer.EXPECT().MarkUploadFailed(ctx, "avatar-2", now).Return(nil)
 
-		uc := NewExpireUploadingAvatars(reader, writer, storage, clock, 30*time.Minute)
+		uc := NewExpireUploadingAvatars(reader, writer, storage, clock, logger.NewNopLogger(), 30*time.Minute)
 		_, err := uc.Execute(ctx, struct{}{})
-		require.EqualError(t, err, "storage unavailable")
+		require.NoError(t, err)
 	})
 }
