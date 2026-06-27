394d8241f84fd8cd7619811ed568e0b5dc835d2b
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-27 13:50:00 +0300

    feat(processor|processing): add presentation use cases port

---
+++ app/internal/processor/processing/presentation/port/usecases.go
@@ -0,0 +1,15 @@
+// Package port defines the presentation-layer contract for processing use cases.
+package port
+
+import (
+	appdto "github.com/iPatrushevSergey/gophprofile/app/internal/processor/processing/application/dto"
+	appport "github.com/iPatrushevSergey/gophprofile/app/internal/processor/processing/application/port"
+)
+
+// ProcessingUseCases provides processing use cases to worker handlers.
+type ProcessingUseCases interface {
+	SubscribeAvatarEventsUseCase() appport.UseCase[struct{}, <-chan appdto.BrokerMessage]
+	ConfirmAvatarEventUseCase() appport.UseCase[appdto.ConfirmAvatarEventInput, struct{}]
+	ProcessUploadedUseCase() appport.UseCase[appdto.ProcessUploadedAvatarInput, struct{}]
+	PurgeDeletedUseCase() appport.UseCase[appdto.PurgeDeletedAvatarInput, struct{}]
+}
