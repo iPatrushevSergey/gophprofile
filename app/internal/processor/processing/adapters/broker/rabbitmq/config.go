4f311fc68f229fd92594c8c75951a727277ad28e
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-28 10:26:06 +0300

    feat(processor|processing): add reconnect interval to rabbitmq consumer config

--- app/internal/processor/processing/adapters/broker/rabbitmq/config.go
+++ app/internal/processor/processing/adapters/broker/rabbitmq/config.go
@@ -3,16 +3,18 @@ package rabbitmq
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 // Config holds RabbitMQ consumer settings.
 type Config struct {
-	URL                  string `mapstructure:"url"`
-	Exchange             string `mapstructure:"exchange"`
-	Queue                string `mapstructure:"queue"`
-	DeadLetterExchange   string `mapstructure:"dead_letter_exchange"`
-	DeadLetterQueue      string `mapstructure:"dead_letter_queue"`
-	DeadLetterRoutingKey string `mapstructure:"dead_letter_routing_key"`
+	URL                  string        `mapstructure:"url"`
+	Exchange             string        `mapstructure:"exchange"`
+	Queue                string        `mapstructure:"queue"`
+	DeadLetterExchange   string        `mapstructure:"dead_letter_exchange"`
+	DeadLetterQueue      string        `mapstructure:"dead_letter_queue"`
+	DeadLetterRoutingKey string        `mapstructure:"dead_letter_routing_key"`
+	ReconnectInterval    time.Duration `mapstructure:"reconnect_interval"`
 }
 
 // Validate trims string fields and checks broker settings when a URL is configured.
@@ -34,6 +36,10 @@ func (c *Config) Validate() error {
 
 	if c.Queue == "" {
 		return fmt.Errorf("broker queue is required")
+	}
+
+	if c.ReconnectInterval <= 0 {
+		return fmt.Errorf("broker reconnect_interval must be positive")
 	}
 
 	return nil
