32d53d0bb0031963c3c3d59cfcdaa6c2f855785b
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-28 22:26:33 +0300

    feat(server|avatar): update AllS3Keys test for multi-format thumbnail keys

--- app/internal/server/avatar/domain/entity/avatar_test.go
+++ app/internal/server/avatar/domain/entity/avatar_test.go
@@ -21,8 +21,10 @@ func TestAvatar_AllS3Keys(t *testing.T) {
 		vo.UploadStatusCompleted,
 		now,
 	)
-	avatar.ThumbnailS3Keys[vo.ThumbnailSize100] = "user-1/avatar-1/100x100"
-	avatar.ThumbnailS3Keys[vo.ThumbnailSize300] = "user-1/avatar-1/300x300"
+	avatar.ThumbnailS3Keys = map[vo.ThumbnailSize]map[vo.OutputFormat]string{
+		vo.ThumbnailSize100: {vo.OutputFormatJPEG: "user-1/avatar-1/100x100"},
+		vo.ThumbnailSize300: {vo.OutputFormatJPEG: "user-1/avatar-1/300x300"},
+	}
 
 	keys := avatar.AllS3Keys()
 
@@ -30,5 +32,4 @@ func TestAvatar_AllS3Keys(t *testing.T) {
 	assert.Contains(t, keys, "user-1/avatar-1/original")
 	assert.Contains(t, keys, "user-1/avatar-1/100x100")
 	assert.Contains(t, keys, "user-1/avatar-1/300x300")
-	assert.Equal(t, "user-1/avatar-1/original", keys[0])
 }
