20569ff0c65bd213134b18096bc7865d02a3f076
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-06-28 23:48:50 +0300

    test(server|avatar): extend avatar entity unit tests

--- app/internal/server/avatar/domain/entity/avatar_test.go
+++ app/internal/server/avatar/domain/entity/avatar_test.go
@@ -33,3 +33,40 @@ func TestAvatar_AllS3Keys(t *testing.T) {
 	assert.Contains(t, keys, "user-1/avatar-1/100x100")
 	assert.Contains(t, keys, "user-1/avatar-1/300x300")
 }
+
+func TestOriginalObjectKey(t *testing.T) {
+	assert.Equal(t, "user-1/avatar-1/original", OriginalObjectKey("user-1", "avatar-1"))
+}
+
+func TestAvatar_LookupVariantKey(t *testing.T) {
+	now := time.Date(2026, 6, 13, 12, 0, 0, 0, time.UTC)
+	avatar := NewAvatar(
+		"avatar-1",
+		"user-1",
+		"avatar.png",
+		"image/png",
+		5,
+		"user-1/avatar-1/original",
+		vo.UploadStatusCompleted,
+		now,
+	)
+	avatar.ThumbnailS3Keys = map[vo.ThumbnailSize]map[vo.OutputFormat]string{
+		vo.ThumbnailSize100: {vo.OutputFormatJPEG: "user-1/avatar-1/100x100/jpeg"},
+	}
+
+	key, mime, ok := avatar.LookupVariantKey(vo.ThumbnailOriginal, "")
+	assert.True(t, ok)
+	assert.Equal(t, "user-1/avatar-1/original", key)
+	assert.Equal(t, "image/png", mime)
+
+	key, mime, ok = avatar.LookupVariantKey(vo.ThumbnailSize100, vo.OutputFormatJPEG)
+	assert.True(t, ok)
+	assert.Equal(t, "user-1/avatar-1/100x100/jpeg", key)
+	assert.Equal(t, "image/jpeg", mime)
+
+	_, _, ok = avatar.LookupVariantKey(vo.ThumbnailSize300, vo.OutputFormatJPEG)
+	assert.False(t, ok)
+
+	_, _, ok = avatar.LookupVariantKey(vo.ThumbnailSize100, vo.OutputFormat("gif"))
+	assert.False(t, ok)
+}
