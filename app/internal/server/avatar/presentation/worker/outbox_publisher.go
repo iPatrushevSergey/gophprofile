78ee1001cb87e034dae5c310852e34f8bdaa7c54
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-07-03 15:14:18 +0300

    Add outbox publisher worker span per polling tick.

--- app/internal/server/avatar/presentation/worker/outbox_publisher.go
+++ app/internal/server/avatar/presentation/worker/outbox_publisher.go
@@ -13,6 +13,7 @@ import (
 type OutboxPublisherWorker struct {
 	useCases presport.AvatarUseCases
 	log      pkgport.Logger
+	tracer   pkgport.Tracer
 	interval time.Duration
 }
 
@@ -20,19 +21,19 @@ type OutboxPublisherWorker struct {
 func NewOutboxPublisherWorker(
 	useCases presport.AvatarUseCases,
 	log pkgport.Logger,
+	tracer pkgport.Tracer,
 	interval time.Duration,
 ) *OutboxPublisherWorker {
 	return &OutboxPublisherWorker{
 		useCases: useCases,
 		log:      log,
+		tracer:   tracer,
 		interval: interval,
 	}
 }
 
 // Run executes the worker loop.
 func (w *OutboxPublisherWorker) Run(ctx context.Context) {
-	uc := w.useCases.PublishPendingOutboxEventsUseCase()
-
 	w.log.Info("outbox publisher worker started", "interval", w.interval)
 
 	ticker := time.NewTicker(w.interval)
@@ -44,9 +45,16 @@ func (w *OutboxPublisherWorker) Run(ctx context.Context) {
 			w.log.Info("outbox publisher worker stopped")
 			return
 		case <-ticker.C:
-			if _, err := uc.Execute(ctx, struct{}{}); err != nil {
+			tickCtx, span := w.tracer.Start(ctx, pkgport.SpanConfig{
+				Key:  "avatar.presentation.outbox_publisher.publish_pending_outbox_events",
+				Name: "publish pending outbox events",
+				Kind: pkgport.SpanKindInternal,
+			})
+			if _, err := w.useCases.PublishPendingOutboxEventsUseCase().Execute(tickCtx, struct{}{}); err != nil {
+				span.Fail(err)
 				w.log.Error("publish pending outbox events failed", "error", err)
 			}
+			span.End()
 		}
 	}
 }
