5ca650a040361aa0532f4a4330472d15e7069829
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-07-04 23:08:46 +0300

    test(server|avatar): wire nop pool stats in avatar use cases test

--- app/internal/server/avatar/application/usecase/usecases_test.go
+++ app/internal/server/avatar/application/usecase/usecases_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/iPatrushevSergey/gophprofile/app/internal/pkg/adapters/logger"
 	pkginmemory "github.com/iPatrushevSergey/gophprofile/app/internal/pkg/adapters/repository/inmemory"
+	postgresadapter "github.com/iPatrushevSergey/gophprofile/app/internal/pkg/adapters/repository/postgres"
 	portmocks "github.com/iPatrushevSergey/gophprofile/app/internal/server/avatar/application/port/mocks"
 )
 
@@ -24,6 +25,7 @@ func TestNewAvatarUseCases_wiresUseCases(t *testing.T) {
 		Transactor:              pkginmemory.NewTransactor(),
 		Clock:                   portmocks.NewMockClock(ctrl),
 		Logger:                  logger.NewNopLogger(),
+		PoolStats:               postgresadapter.NopPoolStats{},
 		OutboxBatchSize:         100,
 		OutboxPublishingTimeout: 5 * time.Minute,
 		UploadReservationTTL:    time.Minute,
@@ -37,4 +39,5 @@ func TestNewAvatarUseCases_wiresUseCases(t *testing.T) {
 	assert.NotNil(t, uc.Health)
 	assert.NotNil(t, uc.ExpireUploadingAvatars)
 	assert.NotNil(t, uc.PublishPendingOutboxEvents)
+	assert.NotNil(t, uc.CollectPeriodicMetrics)
 }
