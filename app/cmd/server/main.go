5087eafcfb760525ad62194027d7dea952d5cbd6
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-07-14 22:14:11 +0300

    refactor(server): own graceful shutdown orchestration in main

--- app/cmd/server/main.go
+++ app/cmd/server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/iPatrushevSergey/gophprofile/app/cmd/server/bootstrap"
 	"github.com/iPatrushevSergey/gophprofile/app/internal/pkg/apputil"
@@ -13,7 +16,27 @@ func main() {
 		return
 	}
 
-	if err := bootstrap.Run(); err != nil {
+	app, cleanups, err := bootstrap.Run()
+	for _, cleanup := range cleanups {
+		defer cleanup()
+	}
+	if err != nil {
+		log.Fatalf("server: %v", err)
+	}
+	if app == nil {
+		return
+	}
+
+	app.Start()
+
+	signalCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	defer stop()
+	<-signalCtx.Done()
+
+	ctx, cancel := context.WithTimeout(context.Background(), app.ShutdownTimeout)
+	defer cancel()
+
+	if err := app.Shutdown(ctx); err != nil {
 		log.Fatalf("server: %v", err)
 	}
 }
